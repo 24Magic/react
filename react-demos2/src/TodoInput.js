34e40c56850683c5de42943165999b3969f4722d
24Magic <15381087533@163.com>
2017-05-04 23:19:06 +0800

    监听回车事件

--- react-demos2/src/TodoInput.js
+++ react-demos2/src/TodoInput.js
@@ -2,6 +2,14 @@ import React, {component} from 'react'
 
 export default class TodoInput extends React.Component {
 	render(){
-		return <input type="text" defaultValue={this.props.content}/>
+		return <input type="text" defaultValue={this.props.content}
+			onKeyPress={this.submit}
+		/>
+	}
+
+	submit(e){
+		if(e.key === 'Enter') {
+			console.log('按下回车')
+		}
 	}
 }
