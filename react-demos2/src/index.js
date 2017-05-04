a32f864c188e8744efa6c988dd36a45d41d723bc
24Magic <15381087533@163.com>
2017-05-04 21:15:14 +0800

    first

--- react-demos2/src/index.js
+++ react-demos2/src/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Welcome from './Welcome';
 import './index.css';
+
+function tick(){
+	ReactDOM.render(
+	  <Welcome name="wind"/>,
+	  document.getElementById('root1')
+	);
+} 
+
+
 
 ReactDOM.render(
   <App />,
   document.getElementById('root2')
 );
+
+// tick()
+
+setInterval(tick, 1000)
