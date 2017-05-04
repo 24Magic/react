85f2f31811d10c7c63e61522fcf3ea9820ddcc24
24Magic <15381087533@163.com>
2017-05-04 23:27:38 +0800

    this.submit.bind(this)

--- react-demos2/src/TodoInput.js
+++ react-demos2/src/TodoInput.js
@@ -3,7 +3,7 @@ import React, {component} from 'react'
 export default class TodoInput extends React.Component {
 	render(){
 		return <input type="text" defaultValue={this.props.content}
-			onKeyPress={this.submit}
+			onKeyPress={this.submit.bind(this)}
 		/>
 	}
 
