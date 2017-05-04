22bc00774e97c8318b2acdf06721c01b1ddaac29
24Magic <15381087533@163.com>
2017-05-04 21:57:54 +0800

    .

--- react-demos2/src/App.js
+++ react-demos2/src/App.js
@@ -1,8 +1,4 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-=======
-
->>>>>>> d522883075bbb9ec7b1effb256d124f9def92f56
 import './App.css';
 import TodoInput from './TodoInput'
 
@@ -18,14 +14,14 @@ class App extends React.Component {
     }
   }
 
-<<<<<<< HEAD
+
   render() {
 
     let todos = this.state.todoList.map((items, index) => {
       return <li>{item.title}</li>
     })
 
-=======
+
 class App extends Component {
   constructor(props){
     super(props)
@@ -42,22 +38,22 @@ class App extends Component {
       return <li>{item.title}</li>
     })
     
->>>>>>> d522883075bbb9ec7b1effb256d124f9def92f56
+
     return (
       <div className="App">
         <h1>我的待办</h1>
         <div className="inputWrapper">
-<<<<<<< HEAD
+
           <TodoInput content={this.state.newTodo}/>
         </div>
         <p className="App-intro">
           To get started, edit<code>src/App.js</code> and save to reload.
-=======
+
         <input type="text" value={this.state.newTodo}/>
         </div>
         <p className="App-intro">
           To get started, edit<code>src/App.js</code> and save to load.
->>>>>>> d522883075bbb9ec7b1effb256d124f9def92f56
+
         </p>
         <ol>
           {todos}
