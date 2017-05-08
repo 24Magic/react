db9b66981ded05490710ea50d1824004ebffa60b
24Magic <15381087533@163.com>
2017-05-09 00:36:41 +0800

    新建leanCloud.js

--- react-demo1/src/App.js
+++ react-demo1/src/App.js
@@ -6,13 +6,6 @@ import TodoInput from './TodoInput'
 import TodoItem from './TodoItem'
 import * as localStorage from './localStorage'
 
-import AV from 'leancloud-storage'
-var APP_ID = '0xiWf8mAkk8MkOG1jE4yYMSm-gzGzoHsz';
-var APP_KEY = 'qKP4VqeNcwTpulqS6Ef5OXfW';
-AV.init({
-  appId: APP_ID,
-  appKey: APP_KEY
-});
 
 
 
---
+++ react-demo1/src/leanCloud.js
@@ -0,0 +1,9 @@
+import AV from 'leancloud-storage'
+var APP_ID = '0xiWf8mAkk8MkOG1jE4yYMSm-gzGzoHsz';
+var APP_KEY = 'qKP4VqeNcwTpulqS6Ef5OXfW';
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+});
+
+export default AV
