4c1d36f057d9ba5984122eaa0e72186689286826
24Magic <15381087533@163.com>
2017-05-11 14:57:21 +0800

    展示友好的错误信息

--- react-demo1/src/UserDialog.js
+++ react-demo1/src/UserDialog.js
@@ -98,7 +98,42 @@ export default class UserDialog extends Component {
 			this.props.onSignUp.call(null, user)
 		}
 		let error = (error)=>{
-			alert('该用户已被注册')
+			switch(error.code){
+				case 200:
+				alert('用户名为空')
+				break
+				case 201:
+				alert('密码为空')
+				break
+				case 202:
+				alert('该用户已被占用')
+				break
+				case 203:
+				alert('邮箱地址已被占用')
+				break
+				case 204:
+				alert('没有提供邮箱地址')
+				break
+				case 205:
+				alert('找不到邮箱地址对应的用户')
+				break
+				case 211:
+				alert('找不到用户')
+				break
+				case 217:
+				alert('无效的用户名，不允许空白用户名')
+				break
+				case 218:
+				alert('无效的密码，不允许空白密码')
+				break
+				case 502:
+				alert('服务器维护中')
+				break
+				default:
+				alert('error')
+				break
+			}
+			
 		}
 		signUp(username, password, email, success, error)
 	}
@@ -109,7 +144,27 @@ export default class UserDialog extends Component {
 			this.props.onSignIn.call(null, user)
 		}
 		let error = (error)=>{
-			alert('该用户已被注册')
+			switch(error.code){
+				case 200:
+				alert('用户名为空')
+				break
+				case 201:
+				alert('密码为空')
+				break
+				case 210:
+				alert('用户名与密码不匹配')
+				break
+				case 211:
+				alert('找不到用户')
+				break
+				case 502:
+				alert('服务器维护中')
+				break
+				default:
+				alert('error')
+				break
+			}
+			
 		}
 		signIn(username, password, success, error)
 	}
