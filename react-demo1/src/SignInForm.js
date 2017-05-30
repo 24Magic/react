ebb38fa96029ed9fdf64755e22b8fa7602b78c7a
24Magic <15381087533@163.com>
2017-05-30 15:04:18 +0800

    将SignIn改成一个函数

--- react-demo1/src/SignInForm.js
+++ react-demo1/src/SignInForm.js
@@ -1,22 +1,20 @@
 import React, {Component} from 'react'
 
-export default class SignInForm extends Component {
-	render () {
-		return (
-			<form className="signUp" onSubmit={this.props.onSubmit.bind(this)}> {/* 登陆*/}
-				<div className="row">					
-					<input placeholder="用户名" type="text" value={this.props.formData.username}
-					 onChange={this.props.onChange.bind(null, 'username')} />
-				</div>
-				<div className="row">					
-					<input placeholder="密码" type="password" value={this.props.formData.password}
-					 onChange={this.props.onChange.bind(null, 'password')} />
-				</div>
-				<div className="row actions">
-					<a href="#" onClick={this.props.onForgotPassword}>忘记密码了?</a>
-					<button type="submit">登陆</button>
-				</div>
-			</form>
-		)
-	}
+export default function(props) {
+	return (
+		<form className="signIn" onSubmit={props.onSubmit.bind(this)}> {/* 登陆*/}
+			<div className="row">					
+				<input placeholder="用户名" type="text" value={props.formData.username}
+				 onChange={props.onChange.bind(null, 'username')} />
+			</div>
+			<div className="row">					
+				<input placeholder="密码" type="password" value={props.formData.password}
+				 onChange={props.onChange.bind(null, 'password')} />
+			</div>
+			<div className="row actions">
+				<a href="#" onClick={props.onForgotPassword}>忘记密码了?</a>
+				<button type="submit">登陆</button>
+			</div>
+		</form>
+	)
 }
