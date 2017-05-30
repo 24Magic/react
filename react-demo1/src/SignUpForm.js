b556c4e029a2165d8351987938553ee10a7610b2
24Magic <15381087533@163.com>
2017-05-30 14:59:15 +0800

    将SignUp改成一个函数

--- react-demo1/src/SignUpForm.js
+++ react-demo1/src/SignUpForm.js
@@ -1,25 +1,23 @@
-import React, {Component} from 'react'
+import React from 'react'
 
-export default class SignUpForm extends Component {
-	render () {
-		return (
-			<form className="signUp" onSubmit={this.props.onSubmit.bind(this)}> {/* 注册*/}
-				<div className="row">					
-					<input placeholder="用户名" type="text" value={this.props.formData.username}
-					 onChange={this.props.onChange.bind(null, 'username')} />
-				</div>
-				<div className="row">					
-					<input placeholder="密码" type="password" value={this.props.formData.password}
-					 onChange={this.props.onChange.bind(null, 'password')} />
-				</div>
-				<div className="row">					
-					<input placeholder="邮箱" type="email" value={this.props.formData.email}
-					 onChange={this.props.onChange.bind(null, 'email')} />
-				</div>
-				<div className="row actions">
-					<button type="submit">注册</button>
-				</div>
-			</form>
-		)
-	}
+export default function(props) {
+	return (
+		<form className="signUp" onSubmit={props.onSubmit.bind(this)}> {/* 注册*/}
+			<div className="row">					
+				<input placeholder="用户名" type="text" value={props.formData.username}
+				 onChange={props.onChange.bind(null, 'username')} />
+			</div>
+			<div className="row">					
+				<input placeholder="密码" type="password" value={props.formData.password}
+				 onChange={props.onChange.bind(null, 'password')} />
+			</div>
+			<div className="row">					
+				<input placeholder="邮箱" type="email" value={props.formData.email}
+				 onChange={props.onChange.bind(null, 'email')} />
+			</div>
+			<div className="row actions">
+				<button type="submit">注册</button>
+			</div>
+		</form>
+	)
 }
